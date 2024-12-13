4960df23a99228d16513d1d307a5312a986d1083
ArpanDhot <sarpanjot@gmail.com>
2024-12-13 16:29:50 +0000

    Implementations: 1- app in gradle:  versionCode = 2 & versionName = "1.2" Bug Fixes:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "ai.humnod.genai.app"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
