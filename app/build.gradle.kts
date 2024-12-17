ed6db3ccef58f4456b2895e5de08218d8617fc9c
ArpanDhot <sarpanjot@gmail.com>
2024-12-17 12:01:15 +0000

    Implementations: 1- app in gradle:  versionCode = 4 & versionName = "1.4" Bug Fixes:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "ai.humnod.genai.app"
         minSdk = 27
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
